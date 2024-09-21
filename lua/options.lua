6e4a4b9c175fe4a5022d9bdc9396a7e4f1b86853
Pato <patricio.moya@krankikom.de>
2024-09-21 15:20:41 +0200

    feature/fine-tuning: adds helper to open terminal in modifiable buffer.. it was modifiable but it did not work out of the box

--- lua/options.lua
+++ lua/options.lua
@@ -62,4 +62,13 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+vim.api.nvim_create_autocmd('TermOpen', {
+  pattern = '*',
+  callback = function()
+    vim.opt_local.modifiable = true
+    vim.opt_local.buflisted = false
+    vim.cmd 'startinsert'
+  end,
+})
+
 -- vim: ts=2 sts=2 sw=2 et
