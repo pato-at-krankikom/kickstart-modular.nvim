ddceaa984bf9641fc4d825bbdec742502d1e5064
Pato <patricio.moya@krankikom.de>
2024-09-21 12:23:35 +0200

    feature/fine-tuning: enables nerd font & adds crtl+y to yank the whole file in normal mode

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -45,4 +45,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Pato's config
+vim.keymap.set('n', '<C-y>', ':%y+<CR>', { desc = 'Yank the whole file' })
+
 -- vim: ts=2 sts=2 sw=2 et
